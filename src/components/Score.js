7fc2da95f8951bf6e8c773cdae722612389b517c
Eugene Obrezkov <ghaiklor@gmail.com>
2017-05-25 14:20:30 +0300

    feature: implement Score component

---
+++ src/components/Score.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#999999',
+    borderRadius: 5,
+    padding: 8
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+});
+
+const mapStateToProps = state => ({
+  score: state.score
+});
+
+export class Score extends React.Component {
+  render() {
+    const {score} = this.props;
+    const text = <Text style={styles.text}>{score}</Text>;
+
+    return <View style={styles.container}>{text}</View>;
+  }
+}
+
+Score.propTypes = {
+  score: PropTypes.number.isRequired
+};
+
+export default connect(mapStateToProps)(Score);
